64c2a9090a3d7090de611552b77633f8727f6bb3
Johnny-K1 <jkosecky54@gmail.com>
2023-02-15 20:44:28 +0100

    change for working with ntb camera

--- main.py
+++ main.py
@@ -1,67 +1,24 @@
-from ximea import xiapi
 import cv2
-### runn this command first echo 0|sudo tee /sys/module/usbcore/parameters/usbfs_memory_mb  ###
 
-#create instance for first connected camera
-cam = xiapi.Camera()
+# create an instance of cv2.VideoCapture() to capture video from the default camera (notebook camera)
+cap = cv2.VideoCapture(0)
 
-
-
-#start communication
-#to open specific device, use:
-#cam.open_device_by_SN('41305651')
-#(open by serial number)
-print('Opening first camera...')
-cam.open_device()
-
-#settings
-cam.set_exposure(10000)
-cam.set_param("imgdataformat","XI_RGB32")
-cam.set_param("auto_wb",1)
-
-print('Exposure was set to %i us' %cam.get_exposure())
-
-#create instance of Image to store image data and metadata
-img = xiapi.Image()
-
-#start data acquisition
-print('Starting data acquisition...')
-cam.start_acquisition()
-
+# set the exposure, image format, and white balance of the camera (if applicable)
+# you may need to adjust these settings based on your camera
+cap.set(cv2.CAP_PROP_EXPOSURE, 0.1)
+# cap.set(cv2.CAP_PROP_CONVERT_RGB, 0.1)
+cap.set(cv2.CAP_PROP_AUTO_WB, 1)
 
 while cv2.waitKey() != ord('q'):
-    cam.get_image(img)
-    image = img.get_image_data_numpy()
-    image = cv2.resize(image,(240,240))
-    cv2.imshow("test", image)
-    cv2.waitKey()
+    # capture a frame from the camera
+    ret, frame = cap.read()
 
-# for i in range(10):
-#     #get data and pass them from camera to img
-#     cam.get_image(img)
-#     image = img.get_image_data_numpy()
-#     cv2.imshow("test", image)
-#     cv2.waitKey()
-#     #get raw data from camera
-#     #for Python2.x function returns string
-#     #for Python3.x function returns bytes
-#     data_raw = img.get_image_data_raw()
-#
-#     #transform data to list
-#     data = list(data_raw)
-#
-#     #print image data and metadata
-#     print('Image number: ' + str(i))
-#     print('Image width (pixels):  ' + str(img.width))
-#     print('Image height (pixels): ' + str(img.height))
-#     print('First 10 pixels: ' + str(data[:10]))
-#     print('\n')
+    # resize the frame (if desired)
+    frame = cv2.resize(frame, (240, 240))
 
-#stop data acquisition
-print('Stopping acquisition...')
-cam.stop_acquisition()
+    # display the frame in a window
+    cv2.imshow("test", frame)
 
-#stop communication
-cam.close_device()
-
-print('Done.')
+# release the camera and close the window
+cap.release()
+cv2.destroyAllWindows()

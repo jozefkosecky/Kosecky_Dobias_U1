636ada6b4dc5d7ad797680cd608523cb9bbcd3fa
Johnny-K1 <jkosecky54@gmail.com>
2023-02-15 20:59:58 +0100

    capturing frames without need to press a key

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ cap.set(cv2.CAP_PROP_EXPOSURE, 0.1)
 # cap.set(cv2.CAP_PROP_CONVERT_RGB, 0.1)
 cap.set(cv2.CAP_PROP_AUTO_WB, 1)
 
-while cv2.waitKey() != ord('q'):
+while True:
     # capture a frame from the camera
     ret, frame = cap.read()
 
@@ -18,6 +18,11 @@ while cv2.waitKey() != ord('q'):
 
     # display the frame in a window
     cv2.imshow("test", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
 
 # release the camera and close the window
 cap.release()
